20b14e2c1168c651460ae5b80bf7fe09f0a5dc64
Emanuel Feld <evonfriedland@users.noreply.github.com>
2015-02-22 17:59:59 -0500

    Update packages.R

--- packages.R
+++ packages.R
@@ -1,23 +1,6 @@
-
-###########################################################
-# R packages to install from CRAN:
-
-my_packages = c("dplyr","RCurl","devtools","BH")
-
-###########################################################
-
-install_if_missing = function(p) {
-  if (p %in% rownames(installed.packages()) == FALSE) {
-    install.packages(p)
-  }
-  else {
-    cat(paste("Skipping already installed package:", p, "\n"))
-  }
-}
-invisible(sapply(my_packages, install_if_missing))
-
-###########################################################
-# Non-CRAN R packages to install:
-
+install.packages("devtools")
 devtools::install_github('rstudio/leaflet')
 devtools::install_github('rstudio/shinydashboard')
+install.packages("dplyr")
+install.packages("RCurl")
+install.packages("BH")
